77c08dc643fadc78d3dc36eacee2357f93786bcd
Nikita Ivanichenko <importantthings1482@gmail.com>
2026-06-09 21:57:05 +0300

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,10 @@
 set -e
 
 echo "Deploying charts"
-helm upgrade --install todoapp ./.infrastructure/helm-charts/todoapp \
-  --values ./.infrastructure/helm-charts/todoapp/values.yaml \
+helm dependency build ./.infrastructure/helm-charts/todoapp/Chart.yaml
+
+helm upgrade --install todoapp ./.infrastructure/helm-chart/todoapp \
+  --values ./.infrastructure/helm-chart/todoapp/values.yaml \
   --namespace todoapp \
   --create-namespace \
   --wait
